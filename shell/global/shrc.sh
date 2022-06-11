33a7c385437c920120770f08ad9c42661a9ba2f3
ethanfogs <fogartyethan@gmail.com>
2022-06-10 23:33:39 -0400

    - Removed the `find . -type ..` from the $FZF_DEFAULT_COMMAND envvar     - It was not working unless "fzf" started the string for the default       command.

--- shell/global/shrc.sh
+++ shell/global/shrc.sh
@@ -8,12 +8,13 @@ export LESSHISTFILE=/dev/null
 
 # FZF -------------------------------------------------------------------------
 
-export FZF_DEFAULT_OPTS="--multi --cycle --reverse --border=rounded"
-export FZF_DEFAULT_COMMAND="find . -type f 2> /dev/null | fzf ${FZF_DEFAULT_OPTS}"
+export FZF_DEFAULT_OPTS="--multi --cycle --reverse --border=rounded --preview='[[ ! -d {} ]] && bat {}'"
+export FZF_DEFAULT_COMMAND="fzf ${FZF_DEFAULT_OPTS}"
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 
 #------------------------------------------------------------------------------
 
 if [[ ${SHLVL} = 1 ]] && [[ $(command -v neofetch) ]]; then neofetch; fi
+
 #------------------------------------------------------------------------------
 # vim: filetype=bash:
