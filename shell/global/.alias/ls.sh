76b437deb850db9f6861d49310ad50ec2f8037a4
ethanfogs <fogartyethan@gmail.com>
2022-06-10 23:28:53 -0400

    - Fixed an '-a' flag error for "ls related" aliases

--- shell/global/.alias/ls.sh
+++ shell/global/.alias/ls.sh
@@ -2,20 +2,15 @@ LS_DEFAULT_PROG=${"$(command -v exa)":-/bin/ls}
 LS_DEFAULT_ARGS='-FG'
 LS_DEFAULT_COMMAND="${LS_DEFAULT_PROG} ${LS_DEFAULT_ARGS}"
 
-[[ "${LS_DEFAULT_PROG}" = '/bin/ls' ]] \
-    && LS_MOD_FLAG_ALL='-a' \
-    || LS_MOD_FLAG_ALL='-A'
-
 #-----------------------------------------------------------------------------
 
 alias ls="${LS_DEFAULT_COMMAND}"
-alias Ls="${LS_DEFAULT_COMMAND} -${LS_MOD_FLAG_ALL}"
+alias Ls="${LS_DEFAULT_COMMAND} -$a"
 
 alias ll="${LS_DEFAULT_COMMAND} -l"
-alias Ll="${LS_DEFAULT_COMMAND} -l"
-alias lL="${LS_DEFAULT_COMMAND} -l${LS_MOD_FLAG_ALL}"
-alias lL="${LS_DEFAULT_COMMAND} -l${LS_MOD_FLAG_ALL}"
+alias lL="${LS_DEFAULT_COMMAND} -la"
+alias Ll="${LS_DEFAULT_COMMAND} -lR"
+alias LL="${LS_DEFAULT_COMMAND} -laR"
 
-alias la="${LS_DEFAULT_COMMAND} -${LS_MOD_FLAG_ALL}"
-alias lA="${LS_DEFAULT_COMMAND} -${LS_MOD_FLAG_ALL}R"
-
+alias la="${LS_DEFAULT_COMMAND} -a"
+alias lA="${LS_DEFAULT_COMMAND} -aR"
