9ec7fdfc2ec1db329ae332819e632df740961863
ethanfogs <fogartyethan@gmail.com>
2022-07-13 16:38:03 -0400

    moved all aliases into a single file (aliasrc.sh)

--- shell/global/aliasrc.sh
+++ shell/global/aliasrc.sh
@@ -1,37 +1,33 @@
-source $HOME/.config/shell/global/.alias/ls.sh
-source $HOME/.config/shell/global/.alias/git.sh
+alias ls="ls -FG"
+alias ll="ls -lFG"
+alias la="ls -aFG"
+alias lla="ls -laFG"
+alias lal="ls -laFG"
 
-#-----------------------------------------------------------------------------
-
-alias cat=${$(command -v bat):-/bin/cat}
-alias grep='grep --color=auto'
-
-#-----------------------------------------------------------------------------
-
-function mcdir(){ mkdir -p "${1}" && cd "${1}"; }
-alias mcd='mcdir'
-
-#-----------------------------------------------------------------------------
+alias mkdir='mkdir -p'
+function mcd(){ mkdir -p "${1}" && cd "${1}"; }
 
 alias chx='chmod +x'
-alias mkx='chmod +x'
 
-##-----------------------------------------------------------------------------
+alias edit=$EDITOR
+alias e=$EDITOR
 
-alias e="${$(command -v nvim):-/usr/bin/vim}"
-
-#-----------------------------------------------------------------------------
-
-function files(){ find ${@:-.} -type f 2> /dev/null | sort; }
+function files(){ find ${@:-.} -not -type d 2> /dev/null | sort; }
 function dirs (){ find ${@:-.} -type d 2> /dev/null | sort; }
 
 alias fs='files'
 alias ds='dirs'
 
-#------------------------------------------------------------------------------
+alias tree='tree -C'
 
-if [[ $(command -v tree) ]]; then
-    TREE_DEFAULT_COMMAND='tree -C --gitignore --dirsfirst'
-    alias tree=$TREE_DEFAULT_COMMAND
-    alias t=$TREE_DEFAULT_COMMAND
-fi
+alias gs="git status"
+alias gl="git log --oneline"
+alias gbr="git branch"
+alias gch="git checkout -b"
+alias gA="git add ."
+alias gc="git commit"
+alias gp="git push"
+alias gps="git push"
+alias gP="git pull"
+alias gpl="git pull"
+alias gm="git merge"
