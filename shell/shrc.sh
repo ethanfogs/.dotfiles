364cd1c0678a59a4ba743db1a1bc1c01d5cce40b
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-17 19:33:19 -0400

    removed HISTSIZE env var, added completion-file sourcing, and added a check for tmux before it starts

--- shell/shrc.sh
+++ shell/shrc.sh
@@ -1,16 +1,22 @@
-# HISTORY MANAGEMENT ----------------------------------------------------------
+# RC\SOURCE FILES -------------------------------------------------------------
 
-export LESSHISTFILE=/dev/null
+source $HOME/.config/shell/aliasrc.sh
+
+# HISTORY MANAGEMENT ----------------------------------------------------------
 
 export APPEND_HISTORY
 export SHARE_HISTORY
 export HIST_VERIFY
-export HISTSIZE=
-export HISTFILE="$XDG_DATA_HOME/${SHELL/*\//}/${SHELL/*\//}_history"
+export HISTFILE="$HOME/.local/share/${SHELL/*\//}/${SHELL/*\//}_history"
 
-# RC\SOURCE FILES -------------------------------------------------------------
+export LESSHISTFILE=/dev/null
 
-source $HOME/.config/shell/aliasrc.sh
+#------------------------------------------------------------------------------
+
+COMPLETION_HOME=/usr/local/share/${SHELL/*\//}/completion
+for COMPLETION_FILE in $(find $COMPLETION_HOME -type f); do
+    source $COMPLETION_FILE
+done
 
 # FZF -------------------------------------------------------------------------
 
@@ -18,8 +24,9 @@ source $HOME/.config/shell/aliasrc.sh
 #export FZF_DEFAULT_COMMAND="fzf ${FZF_DEFAULT_OPTS}"
 #export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 
+# TMUX ON SHELL START-UP ------------------------------------------------------
+
+[ $(command -v tmux) ] && [ -z $TMUX ] && exec tmux
+
 #------------------------------------------------------------------------------
-
-[ -z $TMUX ] && exec tmux
-
 # vim: filetype=bash:
