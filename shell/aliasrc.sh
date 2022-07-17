cb49ab6969cb0001f2ea45fe3ad6e4991f4013af
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-17 13:25:59 -0400

    added some git aliases and organized aliasrc file

--- shell/aliasrc.sh
+++ shell/aliasrc.sh
@@ -1,8 +1,22 @@
+# FILE-SYSTEM - READING -------------------------------------------------------
+
 alias ls="ls -FG"
 alias ll="ls -lFG"
 alias la="ls -aFG"
 alias lla="ls -laFG"
 alias lal="ls -laFG"
+
+function files(){ find ${@:-.} -not -type d 2> /dev/null | sort; }
+function dirs (){ find ${@:-.} -type d 2> /dev/null | sort; }
+alias fs='files'
+alias ds='dirs'
+
+command -v tree 2> /dev/null && alias tree='tree -C'
+
+# FILE-SYSTEM - WRITING -------------------------------------------------------
+
+alias mkdir='mkdir -p'
+function mcd(){ mkdir -p "${1}" && cd "${1}"; }
 
 function trash(){
     for file in $*; do
@@ -12,24 +26,17 @@ function trash(){
 
 alias rm='trash'
 
-alias mkdir='mkdir -p'
-function mcd(){ mkdir -p "${1}" && cd "${1}"; }
-
 alias chx='chmod +x'
+
+# text editor -----------------------------------------------------------------
 
 alias edit=$EDITOR
 alias e=$EDITOR
 
-function files(){ find ${@:-.} -not -type d 2> /dev/null | sort; }
-function dirs (){ find ${@:-.} -type d 2> /dev/null | sort; }
-
-alias fs='files'
-alias ds='dirs'
-
-alias tree='tree -C'
+# GIT -------------------------------------------------------------------------
 
 function git_switch(){
-    [ -z $1 ] && git switch main || git switch $1
+    [ -n $1 ] && git switch $1 || git switch main
 }
 
 alias gs="git status --short"
@@ -46,11 +53,17 @@ alias gsw="git chechout -B"
 alias ga="git add"
 alias gA="git add ."
 alias gc="git commit"
-alias gC="git add . && git commit"
+
+function git_COMMIT(){
+    git add $@ && git commit
+}
+alias gC="git_COMMIT"
+
 alias gp="git push"
 alias gps="git push"
 alias gP="git pull"
 alias gpl="git pull"
 alias gm="git merge"
 
+#------------------------------------------------------------------------------
 # vim: filetype=bash:
