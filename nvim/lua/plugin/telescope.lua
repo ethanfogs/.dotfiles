690e9a9e743bf51e98b10b6a35c4756d455810f6
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-21 00:29:30 -0400

    moved telescope keymaps into telescope config file

--- nvim/lua/plugin/telescope.lua
+++ nvim/lua/plugin/telescope.lua
@@ -1,4 +1,6 @@
-require('telescope').setup({
+local ts = require('telescope')
+
+ts.setup({
     defaults = {
         sorting_strategy = 'ascending',
         winblend = 7,
@@ -28,4 +30,23 @@ require('telescope').setup({
     }
 })
 
--- TELESCOPE KEYMAPS SET @ lua/rc/keymaps.lua
+function ts.set_keymap(lhs, rhs)
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap("n", lhs, "<Cmd>lua require('telescope.builtin')." .. rhs .. "<CR>", opts)
+end
+
+ts.set_keymap("sb", "buffers()")
+ts.set_keymap("sc", "colorscheme({ enable_preview = true })")
+ts.set_keymap("sf", "find_files()")
+ts.set_keymap("sh", "help_tags()")
+ts.set_keymap("sk", "keymaps()")
+ts.set_keymap("sl", "live_grep()")
+ts.set_keymap("sm", "man_pages()")
+ts.set_keymap("so", "vim_options()")
+ts.set_keymap("sq", "quickfix()")
+ts.set_keymap("sr", "registers()")
+ts.set_keymap("sv", "commands()")
+ts.set_keymap("s,", "oldfiles()")
+ts.set_keymap("s/", "current_buffer_fuzzy_find()")
+ts.set_keymap("s;", "command_hist()")
+ts.set_keymap("s?", "builtin()")
