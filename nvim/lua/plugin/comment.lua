bf50e8e8244ca50aa3e4fb06317d5cc77bc19de3
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-22 18:08:07 -0400

    de-bloated

--- nvim/lua/plugin/comment.lua
+++ nvim/lua/plugin/comment.lua
@@ -1,18 +1,1 @@
- require('Comment').setup({
-     pre_hook = function(ctx)
-         local util   = require('Comment.utils')
-         local ts_ctx = require('ts_context_commentstring.utils')
-
-         local location = nil
-
-         if(ctx.ctype == util.ctype.block) then
-            location = ts_ctx.get_cursor_location()
-        elseif((ctx.cmotion == util.cmotion.v) or (ctx.cmotion == util.cmotion.V)) then
-            location = ts_ctx.get_visual_start_location()
-         end
-         return require('ts_context_commentstring.internal').calculate_commentstring({
-             key = ((ctx.ctype == util.ctype.line) and "__default" or "_multiline"),
-             location = location
-         })
-     end,
- })
+require('Comment').setup()
