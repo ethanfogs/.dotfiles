1a5dffe8d9c79a60fc9493dade19f092da683a90
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-25 14:22:42 -0400

    cleaned up file

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,26 +1,21 @@
 local treesitter = require("nvim-treesitter")
 local configs    = require("nvim-treesitter.configs")
 
---local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
---ft_to_parser.motoko = "typescript"
+-- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+-- ft_to_parser.motoko = "typescript"
 
-vim.o.foldmethod = "expr"
-vim.o.foldexpr   = "treesitter#foldexpr()"
+-- vim.o.foldmethod = "expr"
+-- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 configs.setup({
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
+    sync_install = false,
+    ignore_install = { "" },
     highlight = {
         -- use_languagetree = true,
-        enable = true, -- false will disable the whole extension
-        -- additional_vim_regex_highlighting = true,
-    },
-    autopairs = {
         enable = true,
     },
-    indent = {
-        enable = false,
-    },
+    autopairs = { enable = true, },
+    indent = { enable = false, },
     context_commentstring = {
         enable = true,
         enable_autocmd = false,
@@ -31,23 +26,8 @@ configs.setup({
     },
     rainbow = {
         enable = true,
-        colors = {
-            "Gold",
-            "Orchid",
-            "DodgerBlue",
-            -- "Cornsilk",
-            -- "Salmon",
-            -- "LawnGreen",
-        },
+        colors = { "Gold", "Orchid", "DodgerBlue", },
         disable = { "html" },
     },
-    playground = {
-        enable = true,
-    },
-})
-
-require("treesitter-context").setup({
-    enable = false,
-    throttle = true, -- may improve performance
-    max_lines = 0, -- How many lines the window should span. 0 => no limit.
+    playground = { enable = true, },
 })
