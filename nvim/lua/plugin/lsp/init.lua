42001b206a9696649a1717d6350b9948b24d5742
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-25 14:17:37 -0400

    cleaned\formatted file

--- nvim/lua/plugin/lsp/init.lua
+++ nvim/lua/plugin/lsp/init.lua
@@ -1,13 +1,8 @@
-local lsp = {
-    config    = require("lspconfig"),
-    protocol  = vim.lsp.protocol,
-    signature = require('plugin.lsp.lsp-signature'),
-    installer = require("nvim-lsp-installer"),
-    handlers  = {
-        capabilities = vim.lsp.protocol.make_client_capabilities(),
-    },
-}
-
+local lsp     = vim.lsp
+lsp.config    = require("lspconfig")
+lsp.ignature  = require('plugin.lsp.lsp-signature')
+lsp.installer = require("nvim-lsp-installer")
+lsp.handlers  = { capabilities = lsp.protocol.make_client_capabilities() }
 lsp.installer.setup()
 
 function lsp.handlers.on_attach(client, bufnr)
@@ -17,7 +12,7 @@ function lsp.handlers.on_attach(client, bufnr)
     end
 
     buf_set_n_keymap(bufnr, "gC", "<Cmd>lua vim.lsp.buf.code_action()<CR>")
-    buf_set_n_keymap(bufnr, "K",  "<Cmd>lua vim.lsp.buf.hover()<CR>")
+    buf_set_n_keymap(bufnr, "K", "<Cmd>lua vim.lsp.buf.hover()<CR>")
     buf_set_n_keymap(bufnr, "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
     buf_set_n_keymap(bufnr, "gR", "<Cmd>lua vim.lsp.buf.rename(<CR>)<CR>")
     buf_set_n_keymap(bufnr, "gS", "<Cmd>lua vim.lsp.buf.signature_help()<CR>")
@@ -47,8 +42,8 @@ for _, sign in pairs(lsp.signs) do
 end
 
 vim.diagnostic.config({
-    --virtual_text = false,
-    signs = { active = lsp.signs },
+    virtual_text = false,
+    signs = { active = vim.lsp.signs },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
@@ -62,20 +57,19 @@ vim.diagnostic.config({
     }
 })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, {
     border = "rounded",
 })
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, {
     border = "rounded",
 })
 
-lsp.handlers.capabilities = require("cmp_nvim_lsp")
-                                .update_capabilities(lsp.handlers.capabilities)
+lsp.handlers.capabilities = require("cmp_nvim_lsp").update_capabilities(lsp.handlers.capabilities)
 
-for _,server in pairs(lsp.installer.get_installed_servers()) do
+for _, server in pairs(lsp.installer.get_installed_servers()) do
     local import_status, opts = pcall(require, "plugin.lsp.settings." .. server.name)
-    if(not import_status) then opts = {} end
+    if (not import_status) then opts = {} end
 
     opts.on_attach    = lsp.handlers.on_attach
     opts.capabilities = lsp.handlers.capabilities
