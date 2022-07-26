9c7e93083b7a20222f6397348faeeb72f030c108
Ethan Fogarty <fogartyethan@gmail.com>
2022-07-25 22:06:30 -0400

    removed 2 default OSH env variables and cleaned up file

--- shell/bash/bashrc.bash
+++ shell/bash/bashrc.bash
@@ -15,15 +15,12 @@ fi
 # OH-MY-BASH ------------------------------------------------------------------
 
 OSH=$XDG_DATA_HOME/bash/plugins/oh-my-bash
-
 [ ! -d $OSH ] && return
 
 export OSH
 
 OSH_THEME="powerline"
 ENABLE_CORRECTION="true"
-COMPLETION_WAITING_DOTS="true" # display red dots whilst waiting for completion.
-DISABLE_UNTRACKED_FILES_DIRTY="false"
 HIST_STAMPS="mm-dd-yyyy"
 
 OMB_USE_SUDO=true
