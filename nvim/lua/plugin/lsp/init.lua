4658395b790949f69b817fb5a76c2137c9846fd3
ethan <ethanfogs@users.noreply.github.com>
2022-07-30 18:01:47 -0400

    consolidated lsp-signature into lsp-init

--- nvim/lua/plugin/lsp/init.lua
+++ nvim/lua/plugin/lsp/init.lua
@@ -1,10 +1,42 @@
-require('plugin.lsp.lsp-signature')
-require("nvim-lsp-installer").setup()
-
 local lsp = {
     config = require("lspconfig"),
+    signature = require("lsp_signature"),
     handlers  = { capabilities = vim.lsp.protocol.make_client_capabilities() },
 }
+
+require("nvim-lsp-installer").setup()
+
+lsp.signature.config = {
+    debug = false,
+    log_path = "debug_log_file_path",
+    verbose = false,
+    bind = true,
+    doc_lines = 0,
+    floating_window = false,
+    floating_window_above_cur_line = false,
+    fix_pos = false,
+    hint_enable = true,
+    hint_prefix = icons.misc.Squirrel .. " ",
+    hint_scheme = "Comment",
+    use_lspsaga = false,
+    hi_parameter = "LspSignatureActiveParameter",
+    max_height = 12,
+    max_width = 120,
+    handler_opts = { border = "rounded", }, -- double, rounded, single, shadow, none
+    always_trigger = false,
+    auto_close_after = nil,
+    extra_trigger_chars = {},
+    zindex = 200,
+    padding = "",
+    transparency = nil,
+    shadow_blend = 36,
+    shadow_guibg = "Black",
+    timer_interval = 200,
+    toggle_key = nil,
+}
+
+lsp.signature.setup(lsp.signature.config)
+lsp.signature.on_attach(lsp.signature.config)
 
 function lsp.handlers.on_attach(client, bufnr)
     local buf_set_n_keymap = function(bufnr, lhs, rhs)
