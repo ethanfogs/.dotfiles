78458a64525daa54f112867b563970b1bfa62ab8
ethan <ethanfogs@users.noreply.github.com>
2022-07-31 22:16:54 -0400

    moved software installation portions into their own script files

--- installer.zsh
+++ installer.zsh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.{config,cache,local/{bin,share,state}}
 
-[[ $PATH !~ ".*\.local/bin.*" ]] && path+=( $HOME/.local/{s,}bin )
+[[ ! $PATH =~ ".*\.local/bin.*" ]] && path+=( $HOME/.local/{s,}bin )
 
 for dir in $PWD/*; do
     [[ ! $dir =~ ".*\.git.*" ]] && ln -sfv $dir $HOME/.config/$(basename $dir);
@@ -22,53 +22,3 @@ for link in ${links[@]}; do
     ln -sfv $link $HOME/.$ln_name
     [ -h $HOME/.$ln_name/$ln_name ] && unlink $HOME/.$ln_name/$ln_name
 done
-
-if [ $(uname) = "Darwin" ]; then
-
-    homebrew_install(){
-        pkg_category=$*
-
-        if [[ ! $$(command -v brew) ]]; then
-            git clone https://github.com/Homebrew/brew ~/.local/homebrew;
-            ln -sfv ~/.local/homebrew/bin/brew ~/.local/bin
-        fi
-
-        local cli_pkgs=(
-            gnu-sed wget cmake
-            go node yarn python rust sqlite 
-            btop bat exa fd fzf ripgrep tldr tree
-            neovim tmux bitwarden-cli
-            zsh-{completion,autosuggestion}s
-            {bash,brew-cask,pip,rustc,yarn}-completion
-            gobuster httpie httpx john-jumbo nmap openvpn
-            mplayer ffmpeg
-        )
-
-        if [[ $pkg_category =~ ".*cli.*" ]]; then
-            brew install $cli_pkgs --force 2> /dev/null
-        fi
-
-        local desktop_pkgs=(
-            bitwarden amethyst kitty protonvpn firefox brave-browser 
-            wireshark burp-suite
-        )
-
-        if [[ $pkg_category =~ ".*desktop.*" ]]; then
-            mkdir -p $HOME/Applications
-            brew install $desktop_pkgs --cask \
-                --force --no-quarantine --appdir=$HOME/Applications 1> /dev/null
-        fi
-
-        brew update > /dev/null && brew upgrade 2> /dev/null
-    }
-
-    homebrew_install cli desktop
-fi
-
-# curl -sk https://bun.sh/install | bash &> /dev/null \
-# 	&& mv -f ~/.bun ~/.local/share/bun &> /dev/null;
-
-# brew install -f 
-# brew install -f 
-# [ $$(uname -p) = "arm" ] || brew install -f docker{,-machine}
-# brew upgrade
