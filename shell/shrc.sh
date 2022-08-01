ae683135d00937e56eb48b758cd99d33a2debd4e
ethan <ethanfogs@users.noreply.github.com>
2022-08-01 03:58:59 -0400

    added fzf config - fzf works properly when imported as a plugin thru oh-my-zsh

--- shell/shrc.sh
+++ shell/shrc.sh
@@ -16,4 +16,17 @@ export HISTFILE="$HOME/.local/share/${SHELL/*\//}/${SHELL/*\//}_history"
 export LESSHISTFILE=/dev/null
 
 #------------------------------------------------------------------------------
+
+if [[ $(command -v fzf) ]]; then
+    if [[ $(command -v fd) ]]; then
+        export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+    else
+        export FZF_DEFAULT_COMMAND='find . -type f 2> /dev/null'
+    fi
+
+    FZF_PREVIEW="'--preview=[ -f {} ] && cat {}'"
+    export FZF_DEFAULT_OPTS="--multi --cycle --reverse --border=rounded $FZF_PREVIEW"
+fi
+
+#------------------------------------------------------------------------------
 # vim: filetype=bash:
