f97e521aa8e956918f0325e767e6b3fc306f87af
ethan <ethanfogs@users.noreply.github.com>
2022-08-02 19:44:07 -0400

    moved ohmyzsh config into a seprate file and have it sourced into zshrc and added some other "non-pluggin managed" zsh plugins

---
+++ shell/zsh/ohmyzshrc.zsh
@@ -0,0 +1,36 @@
+# OHMYZSH CONFIG -------------------------------------------------------------
+
+ZSH=~/.local/share/ohmyzsh
+
+if [ ! -d $ZSH ]; then
+    git clone --quiet https://github.com/ohmyzsh/ohmyzsh $ZSH
+    source $0
+fi
+
+# COMPLETIONS ----------------------------------------------------------------
+
+completions+=(tmux fzf fd httpie ripgrep ag)
+completions+=(node npm yarn rust aws docker{,-compose,-machine} mongocli)
+for completion in $completions; do
+    [ $(command -v $completion) ] && plugins+=($completion)
+done && completions= && completion=
+
+[ $(command -v go) ] && plugins+=(golang gb)
+[ $(command -v bw) ] && plugins+=(rbw)
+
+ZSH_COMPDUMP=~/.cache/zsh/zcompdump-$SHORT_HOST-$ZSH_VERSION
+autoload -Uz compinit && compinit -d ~/.cache/zsh/zcompdump
+
+# PLUGINS --------------------------------------------------------------------
+
+plugins+=(vi-mode colored-man-pages gnu-utils compleat gitfast) #essentials
+# plugins+=(github git-extras) #git
+# plugins+=(urltools web-search encode64 jsontools) #web-based
+# plugins+=(direnv bgnotify git-auto-fetch shell-proxy torrent z) #experiment with
+
+ZSH_THEME="agnoster"
+
+source $ZSH/oh-my-zsh.sh && plugins=
+
+# delete this completion pattern - it colides with some that ohmyzsh imports
+[ $(command -v mcd) ] && compdef -d mcd
--- shell/zsh/zshrc.zsh
+++ shell/zsh/zshrc.zsh
@@ -1,58 +1,35 @@
+source $HOME/.config/shell/zsh/ohmyzshrc.zsh
 source $HOME/.config/shell/shrc.sh
 
-#------------------------------------------------------------------------------
-
-setopt dotglob
-setopt extendedglob
-setopt menucomplete
-setopt nomatch
-
-setopt nobeep
-setopt chaselinks #go to actual symlink dir rather in cd
-
-bindkey "^K" vi-cmd-mode
-
-#------------------------------------------------------------------------------
-
-# ignore completions for macOS default $HOME directories that I never use
-FIGNORE="sers:cache:ibrary:ublic:ictures:usic:ovies:esktop:ocuments:Trash"
-
-if [[ $(command -v brew) ]]; then
-    [ ! -d ~/Applications ] && mkdir -p ~/Applications
-    export HOMEBREW_CASK_OPTS="-f --no-quarantine --appdir=~/Applications"
-    FPATH="$(brew --prefix)/share/zsh-completions:${FPATH}"
-fi
-
-# ZSH PLUGINS ------------------------------------------------------------
-
-zsh_plugins=(
-    zsh-autosuggestions
-    # zsh-autocomplete      # buggy when zsh-autosuggestions is also applied
+plugins=(
+    zsh-users/zsh-autosuggestions
+    zsh-users/zsh-syntax-highlighting
+    hlissner/zsh-autopair
 )
 
-for plugin in $zsh_plugins; do
-    [ -d $XDG_DATA_HOME/$plugin ] && source "$XDG_DATA_HOME/$plugin/$plugin.zsh"
-done
+for plugin in $plugins; do
+    if [ ! -d ~/.local/share/${plugin/*\/} ]; then
+        git clone https://github.com/$plugin ~/.local/share/${plugin/*\/}
+    fi
 
-# OH-MY-ZSH CONFIG -----------------------------------------------------------
+    source ~/.local/share/${plugin/*\/}/${plugin/*\/}.zsh 2> /dev/null \
+        || source ~/.local/share/${plugin/*\/}/${plugin/*\/}.plugin.zsh
+done && zsh_plugins= && plugin=
 
-autoload -Uz compinit && compinit -d $XDG_CACHE_HOME/zsh/zcompdump
-
-export ZSH=$XDG_DATA_HOME/oh-my-zsh
-if [ ! -d $ZSH ]; then
-    mkdir -p ~/.local/share
-    git clone --quiet https://github.com/ohmyzsh/ohmyzsh $ZSH
+if [ ! $(command -v brew) ] && [ $(uname) = "Darwin" ]; then
+    git clone -q https://github.com/Homebrew/brew ~/.local/homebrew
+    export HOMEBREW_CASK_OPTS="-f --no-quarantine --appdir=~/Applications"
 fi
 
-ZSH_COMPDUMP="$XDG_CACHE_HOME/zsh/zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+#------------------------------------------------------------------------------
 
-ZSH_THEME="agnoster"
+FIGNORE="$FIGNORE:esktop:Trash:ibrary:ictures:terminfo"
 
-plugins=(vi-mode)
-[ $(command -v fzf) ] && plugins+=(fzf)
-[ $(command -v aws) ] && plugins+=(aws)
+setopt dotglob extendedglob nomatch
+setopt menucomplete
+setopt nobeep
+setopt chaselinks # go to actual symlink dir rather in cd
 
-source $ZSH/oh-my-zsh.sh
-
-# delete this completion pattern - colides with some that oh-my-zsh imports 🙄
-[ $(command -v mcd) ] && compdef -d mcd
+bindkey "^H" vi-cmd-mode
+bindkey "^K" up-history
+bindkey "^J" down-history
