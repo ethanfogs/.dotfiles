bb95de05c00224b45050ff0c12c07543e1c3acec
ethan <ethanfogs@users.noreply.github.com>
2022-08-05 03:56:52 -0400

    removed lsp signs - most of them are multi-width chars and cause a weird rendering effect when they'd popup

--- nvim/lua/plugin/lsp/init.lua
+++ nvim/lua/plugin/lsp/init.lua
@@ -25,24 +25,24 @@ function lsp.handlers.on_attach(client, bufnr)
   buf_set_n_keymap(bufnr, "gr", "lsp.buf.references()")
 end
 
-lsp.signs = {
-  { name = "DiagnosticSignError", text = "❌" },
-  { name = "DiagnosticSignWarn", text = "⚠" },
-  { name = "DiagnosticSignHint", text = "💡" },
-  { name = "DiagnosticSignInfo", text = "ℹ️" },
-}
-
-for _, sign in pairs(lsp.signs) do
-  vim.fn.sign_define(sign.name, {
-    texthl = sign.name,
-    text = sign.text,
-    numhl = "",
-  })
-end
+-- lsp.signs = {
+--   { name = "DiagnosticSignError", text = "❌" },
+--   { name = "DiagnosticSignWarn", text = "⚠" },
+--   { name = "DiagnosticSignHint", text = "💡" },
+--   { name = "DiagnosticSignInfo", text = "ℹ️" },
+-- }
+--
+-- for _, sign in pairs(lsp.signs) do
+--   vim.fn.sign_define(sign.name, {
+--     texthl = sign.name,
+--     text = sign.text,
+--     numhl = "",
+--   })
+-- end
 
 vim.diagnostic.config({
   virtual_text = false,
-  signs = { active = vim.lsp.signs },
+  -- signs = { active = vim.lsp.signs },
   update_in_insert = true,
   underline = true,
   severity_sort = true,
@@ -68,7 +68,7 @@ lsp.handlers.capabilities = require("cmp_nvim_lsp")
     .update_capabilities(lsp.handlers.capabilities)
 
 lsp.servers = {
-  'bashls', 'cssls', 'golangci_lint_ls', 'gopls', 'html',
+  'bashls', 'cssls', 'gopls', 'html',
   'jsonls', 'jsonnet_ls', 'marksman', 'pylsp', 'pyright',
   'rust_analyzer', 'sqlls', 'sumneko_lua', 'tsserver', 'vimls'
 }
@@ -91,35 +91,35 @@ end
 
 -- LSP SIGNATURE -------------------------------------------------------------
 
--- lsp.signature = require("lsp_signature")
--- lsp.signature.config = {
---   debug = true,
---   log_path = "debug_log_file_path",
---   verbose = false,
---   bind = true,
---   doc_lines = 0,
---   floating_window = false,
---   floating_window_above_cur_line = false,
---   fix_pos = false,
---   hint_enable = true,
---   hint_prefix = icons.misc.Squirrel .. " ",
---   hint_scheme = "Comment",
---   use_lspsaga = false,
---   hi_parameter = "LspSignatureActiveParameter",
---   max_height = 12,
---   max_width = 120,
---   handler_opts = { border = "rounded", }, -- double, rounded, single, shadow, none
---   always_trigger = false,
---   auto_close_after = nil,
---   extra_trigger_chars = {},
---   zindex = 200,
---   padding = "",
---   transparency = nil,
---   shadow_blend = 36,
---   shadow_guibg = "Black",
---   timer_interval = 200,
---   toggle_key = nil,
--- }
---
--- lsp.signature.setup(lsp.signature.config)
--- lsp.signature.on_attach(lsp.signature.config)
+lsp.signature = require("lsp_signature")
+lsp.signature.config = {
+  debug = true,
+  log_path = "debug_log_file_path",
+  verbose = false,
+  bind = true,
+  doc_lines = 0,
+  floating_window = false,
+  floating_window_above_cur_line = false,
+  fix_pos = false,
+  hint_enable = true,
+  hint_prefix = icons.misc.Squirrel .. " ",
+  hint_scheme = "Comment",
+  use_lspsaga = false,
+  hi_parameter = "LspSignatureActiveParameter",
+  max_height = 12,
+  max_width = 120,
+  handler_opts = { border = "rounded", }, -- double, rounded, single, shadow, none
+  always_trigger = false,
+  auto_close_after = nil,
+  extra_trigger_chars = {},
+  zindex = 200,
+  padding = "",
+  transparency = nil,
+  shadow_blend = 36,
+  shadow_guibg = "Black",
+  timer_interval = 200,
+  toggle_key = nil,
+}
+
+lsp.signature.setup(lsp.signature.config)
+lsp.signature.on_attach(lsp.signature.config)
