be1c75f35c6f3c0b69d6e6a875c01c806716f16e
ethan <ethanfogs@users.noreply.github.com>
2022-08-05 03:55:13 -0400

    removed indentline portions that have unnoticable effects

--- nvim/lua/plugin/indentline.lua
+++ nvim/lua/plugin/indentline.lua
@@ -1,70 +1,28 @@
 local indent_blankline = require('indent_blankline')
 
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-    "help",
-    "startify",
-    "dashboard",
-    "packer",
-    "neogitstatus",
-    "NvimTree",
-    "Trouble",
-}
+vim.g.indent_blankline_filetype_exclude = { "help", "dashboard", "NvimTree", }
 vim.g.indentLine_enabled = 1
--- vim.g.indent_blankline_char = "│"
 vim.g.indent_blankline_char = "▏"
--- vim.g.indent_blankline_char = "▎"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_trailing_blankline_indent = true
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-    "class",
-    "return",
-    "function",
-    "method",
-    "^if",
-    "^while",
-    "jsx_element",
-    "^for",
-    "^object",
-    "^table",
-    "block",
-    "arguments",
-    "if_statement",
-    "else_clause",
-    "jsx_element",
-    "jsx_self_closing_element",
-    "try_statement",
-    "catch_clause",
-    "import_statement",
-    "operation_type",
-}
--- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
--- vim.wo.colorcolumn = "99999"
-
-vim.cmd([[
-    highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine
-    highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine
-    highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine
-    highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine
-    highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine
-    highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine
-]])
+-- vim.g.indent_blankline_show_current_context = true
 
 -- vim.o.list = true
 -- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("space:")
 -- vim.opt.listchars:append("eol:↴")
 
-indent_blankline.setup {
-    -- show_end_of_line = true,
-    -- space_char_blankline = " ",
-    show_current_context = true,
-    -- show_current_context_start = true,
-    -- char_highlight_list = {
-    --   "IndentBlanklineIndent1",
-    --   "IndentBlanklineIndent2",
-    --   "IndentBlanklineIndent3",
-    -- },
-}
+-- vim.cmd([[
+--   highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine
+--   highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine
+--   highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine
+--   highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine
+--   highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine
+--   highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine
+-- ]])
+
+-- indent_blankline.setup {
+--   show_current_context = true,
+--   show_current_context_start = true,
+-- }
