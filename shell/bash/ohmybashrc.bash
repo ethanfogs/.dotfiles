eb227774b01a1f095a16e02784e85e2b7f4acd25
ethan <ethanfogs@users.noreply.github.com>
2022-08-07 13:33:30 -0400

    added some ohmybash env variables

--- shell/bash/ohmybashrc.bash
+++ shell/bash/ohmybashrc.bash
@@ -6,12 +6,19 @@ if [ ! -d $OSH ]; then
   git clone https://github.com/ohmybash/oh-my-bash $OSH
 fi
 
-export CASE_SENSITIVE=false
-export ENABLE_CORRECTION=true
 export OMB_USE_SUDO=false
+export CASE_SENSITIVE=false
+
+export ENABLE_CORRECTION=true
+export DISABLE_LS_COLORS=false
+export COMPLETION_WAITING_DOTS=true
+
 export OSH_THEME="powerline"
+
 export completions=()
-plugins=(git)
+export aliases=()
+export plugins=(git)
+
 which npm >/dev/null && plugins+=(npm)
 which go  >/dev/null && plugins+=(golang goenv)
 which aws >/dev/null && plugins+=(aws)
