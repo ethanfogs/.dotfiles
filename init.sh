c795ee4db8665fa9203ec02dcf74cbbffc806544
ethan <ethanfogs@users.noreply.github.com>
2022-08-07 12:13:02 -0400

    made init.sh is posix compliant

--- init.sh
+++ init.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.{config,cache,local/{bin,share,state}}
+mkdir -p ~/.config ~/.cache ~/.local/share ~/.local/state ~/.local/bin
 
-for dir in *; do
-  [ -h ~/.config/$dir ] && unlink ~/.config/$dir
-  ln -sfv $(pwd)/$dir ~/.config/$dir
+for rc_dir in *; do
+  [ -h "$HOME/.config/$rc_dir" ] && unlink "$HOME/.config/$rc_dir"
+  ln -sfv "$(pwd)/$rc_dir" "$HOME/.config/$rc_dir"
 done
+unset rc_dir
 
-rc_links=(
-  shell/profile.sh
-  shell/bash/bashrc.bash
-  shell/bash/bash_profile.bash
-  shell/zsh/zprofile.zsh
-  shell/zsh/zshrc.zsh
-  shell/inputrc
-  vim
-)
+set -- "shell/profile.sh" "shell/inputrc" \
+       "shell/bash/bash_profile.bash" "shell/bash/bashrc.bash" \
+       "shell/zsh/zprofile.zsh" "shell/zsh/zshrc.zsh" \
+       "vim"
 
-for rc_link in "${rc_links[@]}"; do
-  linkname=$(basename $rc_link | sed 's/\..*//g')
-  [ -h ~/.$linkname ] && unlink ~/.$linkname
-  ln -sfv ~/.config/$rc_link ~/.$linkname
-done; unset rc_links; unset rc_link; unset linkname
+for rc_link in "$@"; do
+  linkname="$(basename $rc_link | sed 's/\..*//g')"
+  [ -h "$HOME/.$linkname" ] && unlink "$HOME/.$linkname"
+  ln -sfv "$HOME/.config/$rc_link" "$HOME/.$linkname"
+done
+unset rc_links; unset rc_link; unset linkname
 
 exec $(ps -p $$ -oargs= | tr -d '-') #returns name of the current shell
