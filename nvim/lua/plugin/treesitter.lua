a9380165c4a2d4e9dd34fa6860ac7007a58c57e9
ethan <ethan.noreply@github.com>
2022-12-21 05:18:09 -0500

    cleaned up

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,21 +1,24 @@
-local treesitter = require("nvim-treesitter")
+local M = {}
 
--- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
--- ft_to_parser.motoko = "typescript"
-
-treesitter.config = {
+M.config = {
   auto_install = true,
   sync_install = false,
   ensure_installed = {
-    "bash", "lua", "python", "sql", "go", "gomod", "gowork", "regex",
-    "vim", "help",
-    "javascript", "tsx", "typescript", "html", "css", "scss",
-    "yaml", "toml", "json", "json5", "jsonc", "jsonnet", "jsdoc", "hjson",
-    "markdown", "markdown_inline", "comment", "todotxt",
-    "dockerfile", "gitattributes", "gitignore", "make", "cmake",
-    "devicetree", "http",
+    "bash",
+    "go",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "python",
+    "regex",
+    "sql",
+    "toml",
+    "typescript",
+    "vim",
+    "yaml",
   },
-  ignore_install = { "phpdoc" },
   -- additional_vim_regex_highlighting = true,
   highlight = {
     use_languagetree = true,
@@ -36,10 +39,13 @@ treesitter.config = {
     colors = { "Gold", "Orchid", "DodgerBlue", },
     disable = { "html" },
   },
-  -- playground = { enable = true, },
+  playground = { enable = true, },
 }
 
-require("nvim-treesitter.configs").setup(treesitter.config)
+-- M.ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+-- M.ft_to_parser.motoko = "typescript"
 
--- vim.o.foldmethod = "expr"
--- vim.o.foldexpr   = "nvim_treesitter#foldexpr()"
+require("nvim-treesitter.configs").setup(M.config)
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr   = "nvim_treesitter#foldexpr()"
