fbf163e86c3b365a00d0ec85c5426f9746824449
ethan <ethan@users.noreply.github.com>
2022-12-23 19:50:27 -0500

    modified preferred colorschemes in nvim config

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,9 @@
 local colorschemes = {
   "base16-material",
-  "base16-onedark",
   "base16-solarflare",
+  "base16-ashes",
+  "base16-ayu-dark",
   "base16-solarized-dark",
-  "base16-da-one-black",
-  -- "base16-ashes"
 }
 
 -- `math.random()` returns last index, using `os.time() % #` as a temp workaround
