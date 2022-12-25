d323d35fb88af528792c2a8959cc7215b2b666d6
ethan <ethan@users.noreply.github.com>
2022-12-25 09:39:59 -0500

    converted arrays to strings and cleaned up the file

--- fzf/fzf.sh
+++ fzf/fzf.sh
@@ -1,58 +1,49 @@
 #!/usr/bin/env bash
 
 FZF_BIN=$(which fzf) || return 1
-__SHELL__=$(ps $$ -oargs= | sed "s/.*[-/]//")
+CURRENT_SHELL=$(ps $$ -oargs= | sed "s/.*[-/]//")
 
-eval "$(cat "${FZF_BIN/bin/opt}/shell/"*".${__SHELL__}" 2>/dev/null)"
+. "${FZF_BIN/bin/opt}/shell/completion.${CURRENT_SHELL}"
+. "${FZF_BIN/bin/opt}/shell/key-bindings.${CURRENT_SHELL}"
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
-FZF_BASE_OPTS=(
-  "--multi"
-  "--cycle"
-  "--sort"
-)
+FZF_FIND="find . -type file -follow 2>/dev/null"
+which fd >/dev/null && FZF_FIND="fd --type=file --follow --hidden"
 
-FZF_LAYOUT_OPTS=(
-  "${FZF_BASE_OPTS[@]}"
-  "--layout=reverse-list"
-  "--border=rounded"
-  "--height=100%"
-)
+which lsd >/dev/null && FZF_LS="lsd"
+FZF_LS="${FZF_LS:-ls} -AF"
 
-FZF_PREVIEW_OPTS=(
-  "${FZF_LAYOUT_OPTS[@]}"
-  "--preview='"
-  "[ -f {} ] && ${FZF_CAT[@]:-cat}  {} && return;"
-  "[ -d {} ] && ${FZF_LS[@]:-ls -l} {} && return;'"
-)
+which bat >/dev/null && FZF_CAT="bat --color=always --theme=Dracula"
+FZF_CAT="${FZF_CAT:-cat}"
 
-FZF_FIND=(
-  "find ."
-  "-not -type d"
-  "2>/dev/null"
-)
+FZF_BASE_OPTS="--multi --cycle --sort --reverse --border=rounded --height=100%"
+FZF_PREVIEW_OPTS="--preview='
+  [ -d {} ] && $FZF_LS  {} && return 0;
+  [ -e {} ] && $FZF_CAT {} && return 0;
+' $FZF_BASE_OPTS"
 
-which fd >/dev/null && export FZF_FIND=(
-  "fd"
-  "--type=file"
-  "--follow"
-)
+#------------------------------------------------------------------------------
 
-which bat >/dev/null && FZF_CAT=(
-  "bat"
-  "--color=always"
-)
+export FZF_DEFAULT_COMMAND="$FZF_FIND"
+export FZF_DEFAULT_OPTS="$FZF_PREVIEW_OPTS"
 
-export FZF_CTRL_R_OPTS=("${FZF_LAYOUT_OPTS[@]}")
-export FZF_CTRL_T_OPTS=("${FZF_PREVIEW_OPTS[@]}")
+export FZF_CTRL_R_COMMAND="fzf-history-widget"
+export FZF_CTRL_R_OPTS="$FZF_BASE_OPTS"
 
-#-------------------------------------------------------------------------------
+export FZF_CTRL_T_COMMAND="$FZF_FIND"
+export FZF_CTRL_T_OPTS="$FZF_PREVIEW_OPTS"
 
-case "${__SHELL__}" in
+#------------------------------------------------------------------------------
+
+case "${CURRENT_SHELL}" in
   zsh)
     bindkey "^F" fzf-file-widget
     ;;
   bash)
     ;;
 esac
+
+#------------------------------------------------------------------------------
+
+unset FZF_BIN FZF_FIND FZF_BASE_OPTS FZF_PREVIEW_OPTS
