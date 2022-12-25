5bddbb3164b0412b147cd70af046e4bd1a73580f
ethan <ethan@users.noreply.github.com>
2022-12-25 09:39:28 -0500

    modularized nvim_tree.lua

--- nvim/after/plugin/nvim_tree.lua
+++ nvim/after/plugin/nvim_tree.lua
@@ -3,85 +3,122 @@ if (not pcall(require, "nvim-tree")) then return end
 local nvim_tree  = setmetatable({}, { __index = require("nvim-tree") })
 nvim_tree.config = setmetatable({}, { __index = require("nvim-tree.config") })
 
-local _, icons = pcall(require, "plugin.icons")
-
-require("nvim-tree").setup({
-  hijack_directories = { enable = false, },
-  hijack_netrw = true,
-  filters = {
-    custom = { ".git" },
-    exclude = { ".gitignore" },
-  },
-  update_cwd = false,
-  renderer = {
-    add_trailing = false,
-    group_empty = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
-    icons = {
-      webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open   = icons.ui.ArrowOpen,
-          arrow_closed = icons.ui.ArrowClosed,
-          default      = icons.documents.Folder or "",
-          open         = icons.documents.OpenFolder or "",
-          empty        = icons.documents.Folder or "",
-          empty_open   = icons.documents.OpenFolder or "",
-          symlink      = icons.documents.Symlink or "",
-          symlink_open = icons.documents.Symlink or "",
-        },
-        git = {
-          unstaged  = icons.git.unstaged or "",
-          staged    = icons.git.staged or "S",
-          unmerged  = icons.git.unmerged or "",
-          renamed   = icons.git.renamed or "➜",
-          untracked = icons.git.untracked or "U",
-          deleted   = icons.git.deleted or "",
-          ignored   = icons.git.Ignore or "◌",
-        },
-      },
-    },
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint    = icons.diagnostics.Hint or "",
-      info    = icons.diagnostics.Information or "",
-      warning = icons.diagnostics.Warning or "",
-      error   = icons.diagnostics.Error or "",
-    },
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    hide_root_folder = false,
-    side = "left",
-    -- auto_resize = true,
-    mappings = {
-      custom_only = false,
-      -- list = {
-      --   { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-      --   { key = "h", cb = tree_cb "close_node" },
-      --   { key = "v", cb = tree_cb "vsplit" },
-      -- },
-    },
-  },
-})
+-------------------------------------------------------------------------------
 
 vim.keymap.set("n", "<Space>e", nvim_tree.toggle, { noremap = true, })
+
+-------------------------------------------------------------------------------
+
+local config = {}
+
+config.hijack_directories = { enable = false, }
+config.hijack_netrw = true
+-- config.update_cwd = false,
+-- auto_resize = true,
+
+config.filters = {
+  custom = { ".git" },
+}
+
+config.git = {
+  enable = true,
+  ignore = true,
+  timeout = 500,
+}
+
+config.view = {
+  side = "left",
+  width = 30,
+  hide_root_folder = false,
+}
+
+config.view.mappings = {
+  custom_only = false,
+  list = {
+    { key = "<CR>",  action = "split" },
+    { key = "<Tab>", action = "tabnew" },
+    { key = "K",     action = "toggle_file_info" },
+    { key = "m",     action = "rename" },
+    { key = "o",     action = "split" },
+    { key = "v",     action = "vsplit" },
+  },
+}
+
+config.renderer = {
+  add_trailing = false,
+  group_empty = false,
+  highlight_git = false,
+  highlight_opened_files = "none",
+}
+
+local has_icons, icons = pcall(require("plugin.icons"))
+
+if has_icons then
+
+  config.diagnostics = {
+    enable = true,
+    icons = {
+      hint    = icons.diagnostics.Hint,
+      info    = icons.diagnostics.Information,
+      warning = icons.diagnostics.Warning,
+      error   = icons.diagnostics.Error,
+    },
+  }
+
+  config.renderer.icons = {
+    webdev_colors = true,
+    git_placement = "before",
+    padding = " ",
+    symlink_arrow = " ➛ ",
+  }
+
+  config.renderer.icons.show = {
+    file = true,
+    folder = true,
+    folder_arrow = true,
+    git = true,
+  }
+
+  config.renderer.icons.glyphs = {
+    default = "",
+    symlink = "",
+    folder = {
+      default      = "",
+      open         = icons.documents.OpenFolder,
+      empty        = icons.documents.Folder,
+    },
+    git = {
+      unstaged  = icons.git.Unstaged,
+      staged    = icons.git.Staged,
+      unmerged  = icons.git.Unmerged,
+      renamed   = icons.git.Renamed,
+      untracked = icons.git.Untracked,
+      deleted   = icons.git.Deleted,
+      ignored   = icons.git.Ignored,
+    },
+  }
+
+  config.renderer.icons.glyphs.folder = {
+    arrow_open   = icons.ui.ArrowOpen,
+    arrow_closed = icons.ui.ArrowClosed,
+    default      = icons.documents.Folder,
+    open         = icons.documents.OpenFolder,
+    empty        = icons.documents.Folder,
+    empty_open   = icons.documents.OpenFolder,
+    symlink      = icons.documents.Symlink,
+    symlink_open = icons.documents.Symlink,
+  }
+
+  config.renderer.icons.glyphs.git = {
+    unstaged  = icons.git.unstaged,
+    staged    = icons.git.staged,
+    unmerged  = icons.git.unmerged,
+    renamed   = icons.git.renamed,
+    untracked = icons.git.untracked,
+    deleted   = icons.git.deleted,
+    ignored   = icons.git.Ignore,
+  }
+
+end
+
+require("nvim-tree").setup(config)
